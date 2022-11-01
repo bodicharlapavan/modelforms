a948121631cb8e9fe9ac727949d89f11653a61c6
bodicharlapavan <bodicharlapavan@gmail.com>
2022-11-01 18:01:35 +0530

    first commit

---
+++ app/forms.py
@@ -0,0 +1,19 @@
+from socket import fromshare
+from django import forms
+from app.models import *
+
+
+class TopicForm(forms.ModelForm):
+    class Meta:
+        model = Topic
+        fields = '__all__'
+
+
+class WebpageForm(forms.ModelForm):
+    class Meta:
+        model = Webpage
+        fields = '__all__'
+        # fields=['topic_name','name']
+        # exclude=['url','name']
+        #help_texts={'name':'enter only alphabets'}
+        widgets = {'name': forms.PasswordInput}
